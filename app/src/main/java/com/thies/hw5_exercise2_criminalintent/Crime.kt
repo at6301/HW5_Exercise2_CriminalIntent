b59299bb6a454c8c3dd082ee908eb470ba639ab0
Alex Thies <at6301@live.missouristate.edu>
2023-04-27 16:03:56 -0500

    Initial commit

---
+++ app/src/main/java/com/thies/hw5_exercise2_criminalintent/Crime.kt
@@ -0,0 +1,10 @@
+package com.thies.hw5_exercise2_criminalintent
+
+import java.util.*
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
